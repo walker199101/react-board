a36958ac96660feb5fe0ecec78ee19adc2d92482
g-kim <walker199101@gmail.com>
2022-05-02 02:25:18 +0900

    WIP 에러 핸들링 추가

--- src/components/container.jsx
+++ src/components/container.jsx
@@ -1,8 +1,21 @@
 import './container.scss';
+import {
+  Alert,
+  AlertTitle
+} from '@mui/material';
+import { useBoardState } from './../boardContext';
 
 function Container({ children }) {
+  const state = useBoardState();
+  const { error } = state;
   return (
     <div className="container">
+      { error && 
+        <Alert severity="error">
+          <AlertTitle>Error</AlertTitle>
+          This is an error alert — <strong>check it out!</strong>
+        </Alert>
+      }
       {children}
     </div>
   );
